b40e697abefa3c02afe8260f8350064d4d67cf33
Ramzi Chahbani <rchahban@c2r5p3.1337.ma>
2025-02-19 11:46:27 +0100

    Chore: removed mb from EditTask page to enhance UX

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -31,7 +31,7 @@ const DashboardLayout = ({ children }) => {
 							<path d="M12 5V19" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
 							<path d="M5 12H19" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
 						</svg>
-					</div> : <NavLink to='/'><div className="flex justify-center items-center bg-teal-600 hover:bg-teal-700 rounded-md p-3 cursor-pointer mb-2">
+					</div> : <NavLink to='/'><div className="flex justify-center items-center bg-teal-600 hover:bg-teal-700 rounded-md p-3 cursor-pointer">
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
 						<Button
 							type="button"
--- frontend/src/pages/EditTask.jsx
+++ frontend/src/pages/EditTask.jsx
@@ -50,14 +50,6 @@ const EditTask = ({ task }) => {
                             classNames="text-white font-semibold text-md ml-2 select-none cursor-pointer"
                         />
                     </div>
-                    {/* <div className="flex justify-center items-center bg-teal-600 hover:bg-teal-700 rounded-md p-3 cursor-pointer mb-2 w-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
-                        <Button
-                            type="button"
-                            text="Return Home"
-                            classNames="text-white font-semibold text-md ml-2 select-none cursor-pointer"
-                        />
-                    </div> */}
                 </div>
 
             </div>
