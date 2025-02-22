0f8cdb523e9a704389a97208b156b6c2cd7cd558
Ramzi Chahbani <rchahban@c2r4p7.1337.ma>
2025-02-23 00:18:27 +0100

    chore: enhanced title for auth testing login

--- backend/tests/integration/auth.test.js
+++ backend/tests/integration/auth.test.js
@@ -47,38 +47,41 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
-test("Returns 401 with 'Invalid email or password.' if user doesn't exist", async () => {
-    let res = await api
-        .post("/api/auth/login")
-        .send({ password: "password123456789", email: "johnDoes@gmail.com" });
-    expect(res.status).toBe(401);
-    expect(res.body.message).toBe("Invalid email or password.");
-});
-test("Returns 400 with 'This email is registered with Google...' if user has googleId", async () => {
-    let res = await api
-        .post("/api/auth/login")
-        .send({ email: "ada@gmail.com", password: "password123456789" });
-    expect(res.status).toBe(400);
-    expect(res.body.message).toBe(
-        `This email is registered with Google. Please use 'Log in with Google'.`
-    );
-});
-test("Returns 401 with 'Invalid email or password.' if password is incorrect", async () => {
-    let res = await api
-        .post("/api/auth/login")
-        .send({ password: "anotherpassword", email: "johnDoes@gmail.com" });
-    expect(res.status).toBe(401);
-    expect(res.body.message).toBe("Invalid email or password.");
-});
-test("Returns 200 with a valid JWT and user data if user logs in successfully", async () => {
-    let res = await api
-        .post("/api/auth/login")
-        .send({ password: "password123456789", email: "johnDoe@gmail.com" });
-    expect(res.status).toBe(200);
-    expect(res.body.success).toBeTruthy();
-    expect(res.body.data).toHaveProperty("token");
-    expect(res.body.data).toHaveProperty("user");
-});
+describe("POST /api/auth/login", () => {
+    test("Returns 401 with 'Invalid email or password.' if user doesn't exist", async () => {
+        let res = await api
+            .post("/api/auth/login")
+            .send({ password: "password123456789", email: "johnDoes@gmail.com" });
+        expect(res.status).toBe(401);
+        expect(res.body.message).toBe("Invalid email or password.");
+    });
+    test("Returns 400 with 'This email is registered with Google...' if user has googleId", async () => {
+        let res = await api
+            .post("/api/auth/login")
+            .send({ email: "ada@gmail.com", password: "password123456789" });
+        expect(res.status).toBe(400);
+        expect(res.body.message).toBe(
+            `This email is registered with Google. Please use 'Log in with Google'.`
+        );
+    });
+    test("Returns 401 with 'Invalid email or password.' if password is incorrect", async () => {
+        let res = await api
+            .post("/api/auth/login")
+            .send({ password: "anotherpassword", email: "johnDoes@gmail.com" });
+        expect(res.status).toBe(401);
+        expect(res.body.message).toBe("Invalid email or password.");
+    });
+    test("Returns 200 with a valid JWT and user data if user logs in successfully", async () => {
+        let res = await api
+            .post("/api/auth/login")
+            .send({ password: "password123456789", email: "johnDoe@gmail.com" });
+        expect(res.status).toBe(200);
+        expect(res.body.success).toBeTruthy();
+        expect(res.body.data).toHaveProperty("token");
+        expect(res.body.data).toHaveProperty("user");
+    });
+})
+
 
 // Refresh Token tests
 
