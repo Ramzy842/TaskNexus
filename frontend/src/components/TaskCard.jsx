e167ffcf6155e8f48f228bc4c2dec05f34618360
Ramzi Chahbani <rchahban@c2r5p5.1337.ma>
2025-03-08 02:55:32 +0000

    Chore: working on drag and drop

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -2,96 +2,119 @@ import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router";
 import { deleteTask, editTask } from "../redux/actions/taskActions";
 import { useEffect } from "react";
+import { Reorder, useDragControls } from "framer-motion";
 
-
-const TaskCard = ({ id, title, status }) => {
-    const dispatch = useDispatch();
-    const tasks = useSelector(state => state.tasks.tasks);
-    useEffect(() => {
-        
-    }, [tasks])
-    const handleDelete = () => {
-        dispatch(deleteTask(id));
-    };
-    const handleCompleted = () => {
-        dispatch(editTask(id, {status: 'Completed'}));
-    }
-    return (
-        <div
-            className={`w-full rounded-sm flex justify-between items-center py-2 px-4 mb-1 shadow-[0_0_8px_-2px_rgba(0,200,200,0.50)] bg-[#E3EAE9] rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-20 border border-gray-100`}
+const TaskCard = ({ id, title, status, task, container }) => {
+  const dispatch = useDispatch();
+  const handleDelete = () => {
+    dispatch(deleteTask(id));
+  };
+  const handleCompleted = () => {
+    dispatch(editTask(id, { status: "Completed" }));
+  };
+  const controls = useDragControls();
+  return (
+    <Reorder.Item
+      dragTransition={{
+        bounceStiffness: 500, // Higher stiffness means less bouncing
+        bounceDamping: 100, // Lower damping results in less snap-back
+      }}
+      value={task}
+      //   dragListener={false}
+      //   dragControls={controls}
+      //   dragConstraints={container}
+      //   dragElastic={0.1}
+      className={`w-full rounded-sm flex justify-between items-center py-2 px-4 mb-1 shadow-[0_0_8px_-2px_rgba(0,200,200,0.50)] bg-[#E3EAE9] bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-20 border border-gray-100`}
+    >
+      <div className="flex flex-col items-start  w-40 sm:w-sm md:w-sm md:w-lg lg:w-2xl">
+        <p
+          className={`sm:hidden select-none mb-1 font-medium ${
+            status === "Completed"
+              ? "bg-teal-600 text-teal-100"
+              : status === "In Progress"
+              ? "bg-blue-600 text-blue-100"
+              : "bg-yellow-600 text-yellow-100"
+          } px-2 py-1 rounded-sm text-xs shadow-sm`}
         >
-            <div className="flex flex-col items-start  w-40 sm:w-sm md:w-sm md:w-lg lg:w-2xl">
-                <p
-                    className={`sm:hidden select-none mb-1 font-medium ${
-                        status === "Completed"
-                            ? "bg-teal-600 text-teal-100"
-                            : status === "In Progress"
-                            ? "bg-blue-600 text-blue-100"
-                            : "bg-yellow-600 text-yellow-100"
-                    } px-2 py-1 rounded-sm text-xs shadow-sm`}
-                >
-                    {status}
-                </p>
-                <p className={`text-sm truncate w-11/12 lg:w-full`}>{title}</p>
-            </div>
-            <div className="flex items-center justify-center ">
-                <p
-                    className={`hidden sm:block font-normal ${
-                        status === "Completed"
-                            ? "bg-teal-600 text-teal-100"
-                            : status === "In Progress"
-                            ? "bg-blue-600 text-blue-100"
-                            : "bg-yellow-600 text-yellow-100"
-                    } px-2 py-1 rounded-sm text-xs shadow-sm`}
-                >
-                    {status}
-                </p>
-                <div className="flex items-center justify-end ml-2 gap-x-2 max-w-30 min-w-30">
-                    <NavLink to={`/tasks/${id}/edit`}>
-                        <img
-                            src="./src/assets/edit.svg"
-                            alt="edit icon"
-                            className="cursor-pointer"
-                        />
-                    </NavLink>
-                    {status !== "Completed" && (
-                        <img
-                            src="./src/assets/check-circle.svg"
-                            alt="mark as completed icon"
-                            className="cursor-pointer"
-                            onClick={handleCompleted}
-                        />
-                    )}
-                    <svg
-                        onClick={handleDelete}
-                        className={`cursor-pointer`}
-                        width="20"
-                        height="20"
-                        viewBox="0 0 20 20"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M15 5L5 15"
-                            stroke={`#579999`}
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2px"
-                        />
-                        <path
-                            d="M5 5L15 15"
-                            stroke={`#579999`}
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2px"
-                        />
-                    </svg>
-                    <img src="/src/assets/drag.svg" className="cursor-pointer"  alt="drag" />
-                    {/* <img src="./src/assets/x.svg" alt="Delete icon" className="cursor-pointer" onClick={() => console.log("Delete")} /> */}
-                </div>
-            </div>
+          {status}
+        </p>
+        <p className={`text-sm text-teal-950 truncate w-11/12 lg:w-full`}>
+          {title}
+        </p>
+      </div>
+      <div className="flex items-center justify-center">
+        <p
+          className={`hidden sm:block font-normal ${
+            status === "Completed"
+              ? "bg-teal-600 text-teal-100"
+              : status === "In Progress"
+              ? "bg-blue-600 text-blue-100"
+              : "bg-yellow-600 text-yellow-100"
+          } px-2 py-1 rounded-sm text-xs shadow-sm mx-2`}
+        >
+          {status}
+        </p>
+        <div className="flex items-center justify-end gap-x-2 max-w-30 min-w-28">
+          <NavLink to={`/tasks/${id}/edit`}>
+            <img
+            //   draggable="false"
+              src="./src/assets/edit.svg"
+              alt="edit icon"
+              className="cursor-pointer"
+            />
+          </NavLink>
+          {status !== "Completed" && (
+            <img
+              draggable="false"
+              src="./src/assets/check-circle.svg"
+              alt="mark as completed icon"
+              className="cursor-pointer"
+              onClick={handleCompleted}
+            />
+          )}
+          <svg
+            draggable="false"
+            onClick={handleDelete}
+            className={`cursor-pointer`}
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M15 5L5 15"
+              stroke={`#579999`}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2px"
+            />
+            <path
+              d="M5 5L15 15"
+              stroke={`#579999`}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2px"
+            />
+          </svg>
+
+          <img
+            src="/src/assets/drag.svg"
+            draggable="false"
+            // onPointerDown={(e) => {
+            //   console.log(e.stopPropagation());
+            //   controls.start(e);
+            // }}
+            className="pointer-events-auto"
+            alt="drag"
+          />
+          {/* <p className="select-none cursor-pointer" >Drag</p> */}
+
+          {/* <img src="./src/assets/x.svg" alt="Delete icon" className="cursor-pointer" onClick={() => console.log("Delete")} /> */}
         </div>
-    );
+      </div>
+    </Reorder.Item>
+  );
 };
 
 export default TaskCard;
--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,13 +1,34 @@
-import TaskCard from "./TaskCard"
+import { useRef, useState } from "react";
+import TaskCard from "./TaskCard";
+import { Reorder } from "framer-motion";
+const TaskList = ({ prevTasks, tasks }) => {
+const [tasksList, setTasksList] = useState(tasks ? tasks : prevTasks);
+//   const container = useRef(null);
+  return tasksList ? (
+    <Reorder.Group
+      axis="y"
+    //   ref={container}
+      values={tasksList}
+      onReorder={setTasksList}
+    >
+      {tasksList.map((task) => {
+        return (
+          <TaskCard
+            // container={container}
+            task={task}
+            key={task.id}
+            id={task.id}
+            title={task.title}
+            status={task.status}
+          />
+        );
+      })}
+    </Reorder.Group>
+  ) : (
+    <h1 className="font-bold text-teal-900 text-xl sm:text-2xl md:text-3xl lg:text-4xl select-none flex justify-center text-center h-full items-center">
+      You Currently have no tasks
+    </h1>
+  );
+};
 
-const TaskList = ({prevTasks, tasks}) => {
-	return prevTasks ? <div>
-	{(tasks ? tasks : prevTasks).map(task => {
-		return <TaskCard key={task.id} id={task.id} title={task.title} status={task.status} />
-	})}
-	</div> : <h1 className="font-bold text-teal-900 text-xl sm:text-2xl md:text-3xl lg:text-4xl select-none flex justify-center text-center h-full items-center">You Currently have no tasks</h1>
-		
-	
-}
-
-export default TaskList
+export default TaskList;
