51755cd6ca5127df95d89d9483a2695231ff8f30
Ramzi Chahbani <rchahban@c2r6p5.1337.ma>
2025-03-19 01:54:16 +0000

    Chore: refactoring

--- frontend/src/components/AccountInfoCard.jsx
+++ frontend/src/components/AccountInfoCard.jsx
@@ -5,17 +5,59 @@ import { useDispatch, useSelector } from "react-redux";
 import { updateUserData } from "../redux/actions/userActions";
 import { SkeletonAccInfoCard } from "./SkeletonSettings";
 
+const Actions = ({
+  handleSave,
+  setIsEditing,
+  setErrors,
+  setEditedCount,
+  setInputVal,
+  originalValue,
+}) => {
+  return (
+    <div className="flex gap-x-2">
+      <div
+        onClick={handleSave}
+        className="flex justify-center items-center bg-cyan-600 hover:bg-cyan-700 rounded-sm py-2 px-4 cursor-pointer"
+      >
+        {/* <img src="/src/assets/save.svg" alt="save" /> */}
+        <Button
+          type="button"
+          text="Save"
+          classNames="text-white font-semibold text-sm  select-none cursor-pointer"
+        />
+      </div>
+      <div
+        onClick={() => {
+          setIsEditing(false);
+          setErrors(null);
+          setEditedCount(0);
+          setInputVal(originalValue);
+        }}
+        className="flex justify-center items-center bg-[#E3123F] hover:bg-red-700 rounded-sm py-2 px-4 cursor-pointer"
+      >
+        {/* <img src="/src/assets/remove-acc.svg" alt="save" /> */}
+        <Button
+          type="button"
+          text="Cancel"
+          classNames="text-white font-semibold text-sm select-none cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+};
+
 const AccountInfoCard = ({ title, value, placeholder, info, setInfo }) => {
-  const originalValue = value;
   const [isEditing, setIsEditing] = useState(false);
   const [editedCount, setEditedCount] = useState(0);
   const [inputVal, setInputVal] = useState(value);
   const [errors, setErrors] = useState(null);
   const error = useSelector((state) => state.user.error);
   const loading = useSelector((state) => state.user.isEditingLoading);
-  const isGoogleAcc = JSON.parse(localStorage.getItem("isGoogleAcc"))
+  const isGoogleAcc = JSON.parse(localStorage.getItem("isGoogleAcc"));
   const dispatch = useDispatch();
-  const cred = useSelector((state) => state.user.user?.[title.toLowerCase()] ?? "");
+  const cred = useSelector(
+    (state) => state.user.user?.[title.toLowerCase()] ?? ""
+  );
 
   const handleSave = () => {
     const updatedField = {};
@@ -49,8 +91,11 @@ const AccountInfoCard = ({ title, value, placeholder, info, setInfo }) => {
     );
     setErrors(errorsArr.length ? errorsArr : null);
   };
-  if ((loading && editedCount)) return <SkeletonAccInfoCard />;
-  return isGoogleAcc && title === "Email" ? <></> : <div>
+  if (loading && editedCount) return <SkeletonAccInfoCard />;
+  return isGoogleAcc && title === "Email" ? (
+    <></>
+  ) : (
+    <div>
       <div
         className={`flex items-center
         mb-2 justify-between`}
@@ -70,40 +115,21 @@ const AccountInfoCard = ({ title, value, placeholder, info, setInfo }) => {
               />
             </div>
           ) : (
-            <p className="text-teal-800  font-normal text-sm truncate">{value}</p>
+            <p className="text-teal-800  font-normal text-sm truncate">
+              {value}
+            </p>
           )}
         </div>
 
         {isEditing ? (
-          <div className="flex gap-x-2">
-            <div
-              onClick={handleSave}
-              className="flex justify-center items-center bg-cyan-600 hover:bg-cyan-700 rounded-sm py-2 px-4 cursor-pointer"
-            >
-              {/* <img src="/src/assets/save.svg" alt="save" /> */}
-              <Button
-                type="button"
-                text="Save"
-                classNames="text-white font-semibold text-sm  select-none cursor-pointer"
-              />
-            </div>
-            <div
-              onClick={() => {
-                setIsEditing(false);
-                setErrors(null);
-                setEditedCount(0);
-                setInputVal(originalValue)
-              }}
-              className="flex justify-center items-center bg-[#E3123F] hover:bg-red-700 rounded-sm py-2 px-4 cursor-pointer"
-            >
-              {/* <img src="/src/assets/remove-acc.svg" alt="save" /> */}
-              <Button
-                type="button"
-                text="Cancel"
-                classNames="text-white font-semibold text-sm select-none cursor-pointer"
-              />
-            </div>
-          </div>
+          <Actions
+            handleSave={handleSave}
+            setIsEditing={setIsEditing}
+            setErrors={setErrors}
+            setEditedCount={setEditedCount}
+            setInputVal={setInputVal}
+            originalValue={value}
+          />
         ) : (
           <div
             onClick={() => {
@@ -121,6 +147,7 @@ const AccountInfoCard = ({ title, value, placeholder, info, setInfo }) => {
           </div>
         )}
       </div>
+
       {errors && (
         <div className="text-xs bg-red-200 text-red-700 py-1 px-2 rounded-sm  relative flex w-full flex-col">
           <span className="self-end bg-red-500 text-white rounded-sm px-1 font-bold text-xs ">
@@ -136,7 +163,7 @@ const AccountInfoCard = ({ title, value, placeholder, info, setInfo }) => {
         </div>
       )}
     </div>
-  
+  );
 };
 
 export default AccountInfoCard;
