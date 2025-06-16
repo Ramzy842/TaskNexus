b5944e66398462858b936b217eb621ea9c4532de
Ramzy842 <ramzychahbani@gmail.com>
2025-06-16 02:54:54 +0100

    Fix: Refined testing endpoint to handle retesting without shutting down the server

--- backend/controllers/testing.js
+++ backend/controllers/testing.js
@@ -1,4 +1,4 @@
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 const Task = require("../models/Task");
 const User = require("../models/User");
 
@@ -9,11 +9,10 @@ testingRouter.post("/reset", async (req, res) => {
     await Task.deleteMany({});
     await Task.collection.drop();
     await User.collection.drop();
-    await mongoose.disconnect();
-    await mongoose.connection.close();
     const { noCreate } = req.body;
-    if (!noCreate)
-    {
+    if (!noCreate) {
+        await mongoose.disconnect();
+        await mongoose.connection.close();
         const testDB_name = `test_db_${Date.now()}`;
         const mongoUri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.qztos.mongodb.net/${testDB_name}?retryWrites=true&w=majority&appName=Cluster0`;
         await mongoose.connect(mongoUri);
